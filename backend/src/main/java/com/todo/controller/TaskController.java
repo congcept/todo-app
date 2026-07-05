c8e55665ad93f589f54de66dcbcefcd7ca4661bf
Công <iamsljacob@gmail.com>
2026-07-05 19:42:44 +0700

    Add CrossOrigin annotation to TaskController

--- backend/src/main/java/com/todo/controller/TaskController.java
+++ backend/src/main/java/com/todo/controller/TaskController.java
@@ -22,9 +22,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(
     path = "/api/tasks",
     produces = MediaType.APPLICATION_JSON_VALUE
